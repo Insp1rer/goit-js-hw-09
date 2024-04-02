5deea2d215f8fd7579fabf66880b94885c681b72
Insp1rer <igorkariymk@gmail.com>
2024-04-02 03:50:07 +0300

    update 2.0

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,30 +1,84 @@
 const storageKey = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
-const textarea = document.querySelector('textarea');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+// --- витягування даних ---
+const savedData = JSON.parse(localStorage.getItem(storageKey)) ?? {};
+emailInput.value = savedData.email ?? '';
+messageInput.value = savedData.message ?? '';
+//--------------------------
 
 form.addEventListener('submit', formSubmitHandler);
 
 function formSubmitHandler(event) {
   event.preventDefault();
-  const text = textarea.value.trim();
-  const name = form.elements.email.value.trim();
-  if (text === '' || name === '') {
+  const email = emailInput.value.trim();
+  const message = messageInput.value.trim();
+  if (email === '' || message === '') {
     alert('Заповніть поля');
     return;
   }
-  const userData = JSON.stringify({ text, name });
-  localStorage.setItem(storageKey, userData);
+  const userData = {
+    email,
+    message,
+  };
+
+  localStorage.setItem(storageKey, JSON.stringify(userData));
   form.reset();
+  console.log(userData);
+  localStorage.clear();
 }
 
-const json = localStorage.getItem(storageKey) ?? '';
+// --- зберігання даних ---
+form.addEventListener('input', () => {
+  const email = emailInput.value.trim();
+  const message = messageInput.value.trim();
+  const userData = {
+    email,
+    message,
+  };
+  localStorage.setItem(storageKey, JSON.stringify(userData));
+});
 
-try {
-  const data = JSON.parse(json);
-  console.log(data);
-  textarea.value = data.text;
-  form.elements.email.value = data.name;
-} catch {
-  console.log('no user data');
-}
+// const storageKey = 'feedback-form-state';
+
+// const form = document.querySelector('.feedback-form');
+// const emailInput = form.elements.email;
+// const messageInput = form.elements.message;
+
+// form.addEventListener('submit', formSubmitHandler);
+
+// function formSubmitHandler(event) {
+//   event.preventDefault();
+//   const email = emailInput.value.trim();
+//   const message = messageInput.value.trim();
+//   if (email === '' || message === '') {
+//     alert('Заповніть поля');
+//     return;
+//   }
+//   const userData = {
+//     email,
+//     message,
+//   };
+//   localStorage.setItem(storageKey, JSON.stringify(userData));
+
+//   form.reset();
+
+//   console.log(userData);
+
+//   localStorage.clear();
+// }
+
+// const json = localStorage.getItem(storageKey) ?? '';
+
+// try {
+//   const data = JSON.parse(json);
+//   messageInput.value = data.message;
+//   emailInput.value = data.email;
+// } catch {
+//   console.log('no user data');
+// }
+
+//-----------------------------------------------------------------------
